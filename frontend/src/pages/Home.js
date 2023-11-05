43215fa45eda1a224c1014c210d59b6020fd4dde
selwyn25landayan <selwynlandayan25@gmail.com>
2023-11-05 19:32:13 +0800

    New Frontend

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,36 +1,34 @@
 import { useEffect } from "react";
 import { useOrderDetailsContext } from "../hooks/useOrderDetailsContext";
-
 import OrderDetails from "../components/OrderDetail";
 import WorkoutForm from "../components/OrderDetailForm";
 
-
 const Home = () => {
-  const {orderdetails, dispatch} = useOrderDetailsContext()
+  const { orderdetails, dispatch } = useOrderDetailsContext();
 
   useEffect(() => {
     const fetchOrderDetails = async () => {
-      const response = await fetch('/api/orderDetails/'); 
+      const response = await fetch('/api/orderDetails/');
       const json = await response.json();
 
       if (response.ok) {
-        dispatch({type: 'SET_ORDERDETAILS', payload: json})
+        dispatch({ type: 'SET_ORDERDETAILS', payload: json });
       }
     };
     fetchOrderDetails();
-  }, []);
+  }, [dispatch]); // Add 'dispatch' to the dependency array
 
   return (
     <div className="home">
-    <div className="orderdetails-container">
-      <WorkoutForm />
-    </div>
-    <div className="orderdetails">
-      {orderdetails && orderdetails.map((orderdetail) => (
+      <div className="orderdetails-container">
+        <WorkoutForm />
+      </div>
+      <div className="orderdetails">
+        {orderdetails && orderdetails.map((orderdetail) => (
           <OrderDetails orderdetail={orderdetail} key={orderdetail._id} />
         ))}
+      </div>
     </div>
-  </div>  
   );
 };
 
